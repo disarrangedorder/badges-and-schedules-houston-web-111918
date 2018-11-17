2d5b0c63bf64aa20e8b87fd11bf136a3a48f3994
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 17:47:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+speakers = ["Edsger", "Ada", "Charles", "Grace", "Linus", "Matz"]
+
+def badge_maker (name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator (name_list)
+  
+  batch_badges = []
+
+  name_list.each do |name|
+    batch_badges.push(badge_maker)
+  end
+  
+end
+
+
+
+
+
+
+def assign rooms (speakers, room)
+  
