c2c8767ba5022421395adfc105669d9f6e7d0e8b
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 18:08:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,10 +19,7 @@ def assign_rooms (attendees)
 
   room_assignments = []
   i = 1
-  attendees.each do |attendee|
-    room_assignments.push("Hello, #{"attendee"}! You'll be assigned to room #{i}!")
-    i++
-  end
+
 end
 
 
