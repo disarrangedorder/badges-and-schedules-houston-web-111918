8cf5629595697e9104c0ac3ae5f267e7a7c7de13
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 18:28:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,10 +29,10 @@ end
 
 
 def printer (attendees)
-  batch_badge_creator.each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  assign_rooms.each do |assignment|
+  assign_rooms(attendees).each do |assignment|
     puts assignment
   end
 end
